bd36fdd23c7e15d29fdc4b1b13855ab14ce51896
oluwadolz <olaolushesi98@gmail.com>
2020-03-22 19:22:17 +0100

    :hankey: Ft: added the navigation items and sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from "react";
 import Body from '../../hoc/Body'
 import "./Layout.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 const layout = (props) =>(
         <Body>
-            <Toolbar/>
+            <Toolbar />
+            <SideDrawer />
             <main className="content">{props.children}</main>
         </Body>
 )
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./NavigationItems.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const navigationItems = () =>(
+    <ul className="NavigationItems">
+       <NavigationItem link="/" active>Burger Builder</NavigationItem>
+       <NavigationItem>Checkout</NavigationItem>
+    </ul>
+)
+
+export default navigationItems
