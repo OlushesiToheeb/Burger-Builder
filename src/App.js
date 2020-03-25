fd146ee8bd1c15fe983c0a0d82536a4768ad06e6
oluwadolz <olaolushesi98@gmail.com>
2020-03-25 18:59:28 +0100

    :flushed: Fix: eject interceptors

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Layout from "./components/Layout/Layout"
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
 
-function App() {
-  return (
-    <div >
+class App extends React.Component{
+ 
+
+  render(){
+    return(
+      <div >
       <Layout>
-         <BurgerBuilder/>
+        <BurgerBuilder/>
       </Layout>
       
     </div>
-  );
+    )
+  }
 }
 
 export default App;
