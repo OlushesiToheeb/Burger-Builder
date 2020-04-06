a2a02e99e5e2078abf17c08e9b8ad082a9e597f4
OlushesiToheeb <olaolushesi98@gmail.com>
2020-04-06 23:49:04 +0100

    :punch: ft - handled the input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,19 +10,22 @@ const input = (props) =>{
            inputElement =<input  
                 className='InputElement' 
                 {...props.elementConfig} 
-                value={props.value}/>
+                value={props.value}  
+                onChange={props.changed}/>
            break;
         case('textarea'):
             inputElement=<textarea  
                 className='InputElement' 
                 {...props.elementConfig} 
-                value={props.value}/>
+                value={props.value}  
+                onChange={props.changed}/>
             break;
         case('select'):
             inputElement = ( 
                 <select  
                     className='InputElement' 
-                    value={props.value}>
+                    value={props.value} 
+                    onChange={props.changed}>
                       {props.elementConfig.options.map(option => (
                           <option key={option.value} 
                             value={option.value}>
@@ -34,7 +37,10 @@ const input = (props) =>{
             break;
         default:
             inputElement = <input  
-            className='InputElement' {...props.elementConfig} value={props.value}/>
+                className='InputElement' 
+                {...props.elementConfig} 
+                value={props.value} 
+                onChange={props.changed}/>
     }
 
     return(
