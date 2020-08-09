6b176397faa90b33444c50e9e7d431cfa79c63ac
OlushesiToheeb <olaolushesi98@gmail.com>
2020-08-09 03:38:38 +0100

    added a spinner and show error message

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import './Auth.css';
 import { connect } from 'react-redux';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import * as actions from '../../store/actions';
 
 class Auth extends Component {
@@ -118,8 +119,18 @@ class Auth extends Component {
         changed={(event)=>this.inputChangedHandler(event, formElement.id)}/>
     ))
 
-    return(
+    let errorMessage = null;
+    if(this.props.error){
+      errorMessage = (
+        <p>{this.props.error.message}</p>
+      )
+    }
+
+    return this.props.loading ? 
+      <Spinner/> :
+      (
       <div className="Auth">
+        {errorMessage}
         <form onSubmit={this.submitHandler}>
           {authForm }
           <Button btnType ='success'>SUBMIT</Button>
@@ -136,10 +147,17 @@ class Auth extends Component {
   }
 }
 
+const mapStateToProps = (state) =>{
+  return {
+    loading : state.auth.loading,
+    error : state.auth.error
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return{
     onAuth : (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
   }
 }
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
