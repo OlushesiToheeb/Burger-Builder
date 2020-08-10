94b5896ec8b57e5372866d991c49453a780b4982
OlushesiToheeb <olaolushesi98@gmail.com>
2020-08-10 18:51:46 +0100

    forwarding Unauthenticated users

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -46,7 +46,9 @@ class BurgerBuilder extends Component{
 
 
     purchaseHandler = () =>{
-        this.setState({purchasing:true})
+        this.props.isAuthenticated ?
+        this.setState({purchasing:true}) :
+        this.props.history.push('/auth');
     }
 
     updatePurchaseState(ingredients){
@@ -87,7 +89,10 @@ class BurgerBuilder extends Component{
                         disabled={disabledInfo}
                         purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
-                        price={this.props.price}/>
+                        price={this.props.price}
+                        isAuth={this.props.isAuthenticated}
+                        />
+                        
                 </Body>
             );
 
@@ -121,7 +126,8 @@ const mapStateToProps = (state) => {
     return{
         ings: state.burgerBuilder.ingredients,
         price : state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated : state.auth.token !== null
     }
 }
 
