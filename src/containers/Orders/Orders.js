07826853fb35c9b33f7bf07e71f374e0ddab7a89
OlushesiToheeb <olaolushesi98@gmail.com>
2020-08-11 03:42:10 +0100

    displaying user specific order

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ import * as actions from '../../store/actions/index';
 class Orders extends React.Component{
 
     componentDidMount(){
-       this.props.onFetchorders(this.props.token)
+       this.props.onFetchorders(this.props.token, this.props.userId)
     }
 
     render(){
@@ -41,13 +41,14 @@ const mapStateToProps = state => {
     return{
         orders : state.order.orders,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId : state.auth.userId
     }
 }
 
 const mapDispatchToProps = dispatch =>{
     return{
-        onFetchorders: (token) => dispatch(actions.fetchOrders(token)),
+        onFetchorders: (token, userId) => dispatch(actions.fetchOrders(token, userId)),
     }
 }
 
